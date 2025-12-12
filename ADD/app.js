ce9d92dd1178049216fbdfe8f779bc653b386aa6
eieggzs <DJCoronaGomez@gmail.com>
2025-12-12 07:48:27 -0600

    ajuste7

--- ADD/app.js
+++ ADD/app.js
@@ -127,11 +127,11 @@ async function actualizar() {
     if (!sensores) {
         console.warn("Sensores no disponibles, usando últimos valores");
     } else {
-        tempActual = sensores.temperatura ?? tempActual;
-        humActual = sensores.humedad ?? humActual;
-        metanoActual = sensores.metano ?? metanoActual;
-        pesoActual = sensores.peso ?? pesoActual;
-        distActual = sensores.movimiento ?? distActual;
+        tempActual = sensores.Temperatura ?? tempActual;
+        humActual = sensores.Humedad ?? humActual;
+        metanoActual = sensores.Metano ?? metanoActual;
+        pesoActual = sensores.Peso ?? pesoActual;
+        distActual = sensores.Movimiento ?? distActual;
     }
 
     await obtenerPrediccion();
