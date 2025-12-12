ddbbb3f00815f5e5f3cb8c4fb722050e463c957a
eieggzs <DJCoronaGomez@gmail.com>
2025-12-12 04:15:27 -0600

    cambios finales

--- server.py
+++ server.py
@@ -1,42 +1,40 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-# Servir carpeta ADD (frontend)
+# Servir carpeta ADD
 app.mount("/ADD", StaticFiles(directory="ADD"), name="ADD")
 
+# Servir index.html en raíz
 @app.get("/")
 def home():
     return FileResponse("ADD/index.html")
 
-# Cargar modelo
+# Cargar modelo ML
 model = joblib.load("modelo_biodegradacion.pkl")
 
-# Input correcto (coincide con dataset_final y tu modelo ML)
 class InputData(BaseModel):
-    temperatura: float
-    humedad: float
-    metano: float
-    peso: float
-    movimiento: float
-    tiempo_horas: float   # IMPORTANTE para la degradación
+    Temperatura: float
+    Humedad: float
+    Metano: float
+    Peso: float
+    Distancia: float
 
 @app.post("/predict")
 def predict(data: InputData):
 
     entrada = [[
-        data.temperatura,
-        data.humedad,
-        data.metano,
-        data.peso,
-        data.movimiento,
-        data.tiempo_horas
+        data.Temperatura,
+        data.Humedad,
+        data.Metano,
+        data.Peso,
+        data.Distancia
     ]]
 
     pred = model.predict(entrada)[0]
+
     return {"nivel_degradacion": float(pred)}
